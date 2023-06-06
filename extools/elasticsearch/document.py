de7e58710493fe74e0b2d3e3af988cb2f1dcf6ac
yigerenwan <excitonx@gmail.com>
2023-06-06 20:23:12 +0000

    update

--- extools/elasticsearch/document.py
+++ extools/elasticsearch/document.py
@@ -1,6 +1,12 @@
 from typing import Any, Dict
 
 from elasticsearch import Elasticsearch
+import urllib3
+from typing import Any, Dict, List
+
+import pika
+
+urllib3.disable_warnings()
 
 def get_all_docs(
     client: Elasticsearch,
@@ -8,6 +14,7 @@ def get_all_docs(
     query: Dict[str, Any] = {"match_all": {}},
     get_source: bool = True,
 ) -> Dict[str, Any]:
+    
     old_scroll_id = None
     resp = client.search(
         index=index, query=query, size=200, scroll="30s", ignore=[400, 404]
@@ -31,3 +38,37 @@ def get_all_docs(
         old_scroll_id = resp["_scroll_id"]
         if old_scroll_id != resp["_scroll_id"]:
             print("NEW SCROLL ID:", resp["_scroll_id"])
+
+
+def send_messages_to_exchange(
+    host: str,
+    port: str,
+    virtual_host: str,
+    username: str,
+    password: str,
+    exchange_name: str,
+    exchange_type: str,
+    routing_key: str,
+    messages: List[str],
+):
+    # rabbitmq
+    credentials = pika.PlainCredentials(username=username, password=password)
+    connectparam = pika.ConnectionParameters(
+        host=host,
+        port=port,
+        virtual_host=virtual_host,
+        credentials=credentials,
+    )
+    connection = pika.BlockingConnection(connectparam)
+    channel = connection.channel()
+    channel.exchange_declare(
+        exchange=exchange_name,
+        exchange_type=exchange_type,
+    )
+    for message in messages:
+        channel.basic_publish(
+            exchange=exchange_name,
+            routing_key=routing_key,
+            body=message,
+        )
+    connection.close()
