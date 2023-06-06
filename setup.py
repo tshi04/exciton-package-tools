ccf7fea1ac97a164db621e1556145e918cac7880
yigerenwan <excitonx@gmail.com>
2023-06-06 20:02:44 +0000

    Bump version: 2.0.7 → 2.0.8

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open("requirements.txt", "r") as fp:
         if line[0] != "#":
             dep_packages.append(str(line.strip()))
 
-__version__ = "2.0.7"
+__version__ = "2.0.8"
 
 setup(
     name="extools",
