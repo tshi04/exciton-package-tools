deaeb59ffdc8efda4eae965a6d01ea4f4912241e
yigerenwan <excitonx@gmail.com>
2024-06-28 01:51:10 +0000

    Bump version: 0.5.0 → 0.5.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open("requirements.txt", "r") as fp:
         if line[0] != "#":
             dep_packages.append(str(line.strip()))
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 setup(
     name="exciton_tools",
